10e3320e8139378d95143339c738f63de89acc08
Gabriella <gabslima1111@gmail.com>
2025-10-02 13:42:18 -0300

    Exercícios 1-45

--- Lote.01/ex-4.py
+++
@@ -1,8 +0,0 @@
-#Que tal converter uma temperatura de graus celsius
-#para uma temperatura em graus fairenheit?
-
-celsius = float(input("Digite o valor da temperatura em graus celsius: "))
-
-fahrenheit = (9*celsius+160) / 5
-
-print("A sua temperatura convertida de graus celsius para graus fahrenheit é: ", fahrenheit)
