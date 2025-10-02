8e9740de00b28ee18ba31ec1662182d3b943db54
Gabriella <gabslima1111@gmail.com>
2025-10-02 13:39:44 -0300

    Delete Lote.01/ex-3.py

--- Lote.01/ex-3.py
+++
@@ -1,8 +0,0 @@
-#Vamos descobrir a área de um triângulo?
-
-base = float(input("Digite o valor da base do triângulo: "))
-altura = float(input("Digite o valor da altura de seu triângulo: "))
-
-area = (base * altura) / 2
-
-print("A área de seu triângulo é: ", area)
