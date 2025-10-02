0dfa95a33787ebe2290a06b5668b4c3b5be2e5b2
Gabriella <gabslima1111@gmail.com>
2025-10-02 13:41:12 -0300

    Exercícios 16-45

--- Lote.01/ex-9.py
+++
@@ -1,8 +0,0 @@
-#Vamos descobrir a soma entre dois números ao quadrado?
-
-numeroUm = float(input("Digite o valor do primeiro número: "))
-numeroDois = float(input("Digite o valor do segundo número: "))
-
-soma = ((numeroUm*numeroUm)+(numeroDois*numeroDois))
-
-print("O valor da soma dos quadrados dos números recebidos é: ", soma)
