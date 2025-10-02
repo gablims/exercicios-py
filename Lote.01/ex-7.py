01c4fa090c2e68b5bfedb090d796c47163205223
Gabriella <gabslima1111@gmail.com>
2025-10-02 13:40:20 -0300

    Delete Lote.01/ex-7.py

--- Lote.01/ex-7.py
+++
@@ -1,9 +0,0 @@
-#Vamos descobrir o volume de um paralelepípedo
-
-comprimento = float(input("Digite o valor do comprimento de seu paralelepípedo: "))
-altura = float(input("Digite o valor da altura de seu paralelepípedo: "))
-largura = float(input("Digite o valor da largura de seu paralelepípedo: "))
-
-volume = comprimento*altura*largura
-
-print("O volume de seu paralelepípedo é: ", volume)
